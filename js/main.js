5a84902149c2b4c9aeb1ac5ddc467be07bcb9bcc
cvines528 <cvines528@gmail.com>
2020-08-17 11:05:15 -0400

    added functionality to multiple operations being calculated without having to clear entire calculator

--- js/main.js
+++ js/main.js
@@ -16,22 +16,58 @@ keys.addEventListener('click', e => {
             } else {
                 display.textContent = displayedNum + keyContent;
             }
+
+            calculator.dataset.previousKeyType = 'number'
         }
 
         if(action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
+            const firstValue = calculator.dataset.firstValue;
+            const operator = calculator.dataset.operator;
+            const secondValue = displayedNum;
+            const calculate = (n1, operator, n2) => {
+                let result = '';
+
+                if (operator === 'add') {
+                    result = parseFloat(n1) + parseFloat(n2);
+                } else if (operator === 'subtract') {
+                    result = parseFloat(n1) - parseFloat(n2);
+                } else if (operator === 'multiply') {
+                    result = parseFloat(n1) * parseFloat(n2);
+                } else if (operator === 'divide') {
+                    result = parseFloat(n1) / parseFloat(n2);
+                }
+
+                return result
+            }
+
+            if (firstValue && operator && previousKeyType !== 'operator') {
+                const calcValue = calculate(firstValue, operator, secondValue)
+                display.textContent = calcValue
+
+                // Update calculated value as firstValue
+                calculator.dataset.firstValue = calcValue
+            } else {
+                // If there are no calculations, set displayedNum as the firstValue
+                calculator.dataset.firstValue = displayedNum
+            }
+
             key.classList.add('is-depressed');
-            calculator.dataset.firstValue = displayedNum
             calculator.dataset.operator = action
-            // Add custom attribute
             calculator.dataset.previousKeyType = 'operator'
         }
 
         if (action === 'decimal') {
+            if (!displayedNum.includes('.')){
             display.textContent = `${displayedNum}.`
+            } else if (previousKeyType === 'operator') {
+                display.textContent = '0.'
+            }
+
+            calculator.dataset.previousKeyType = 'decimal'
         }
 
         if (action === 'clear') {
-            console.log('clear key!');
+            calculator.dataset.previousKeyType = 'clear'
         }
 
         if (action === 'calculate') {
@@ -53,9 +89,15 @@ keys.addEventListener('click', e => {
 
                 return result
             }
-
-            display.textContent = calculate(firstValue, operator, secondValue)
+            
+            if (firstValue) {
+                display.textContent = calculate(firstValue, operator, secondValue)              
+            }
+            
+            calculator.dataset.previousKeyType = 'calculate'
         }
+
+        
 
         // Removal of .is-depressed class from all keys
         Array.from(key.parentNode.children)
