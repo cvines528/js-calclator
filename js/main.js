42c1e430fc21a83f59196f08a038ad427c23f690
cvines528 <cvines528@gmail.com>
2020-08-17 11:47:26 -0400

    added clearing functionality. Calculator should work as intended.  refractory of code needs to happen soon.

--- js/main.js
+++ js/main.js
@@ -65,7 +65,22 @@ keys.addEventListener('click', e => {
             calculator.dataset.previousKeyType = 'decimal'
         }
 
+        if(action !== 'clear') {
+            const clearButton = calculator.querySelector('[data-action=clear]');
+            clearButton.textContent = 'CE'
+        }
+
         if (action === 'clear') {
+            if (key.textContent === 'AC') {
+                calculator.dataset.firstValue = ''
+                calculator.dataset.modValue = ''
+                calculator.dataset.operator = ''
+                calculator.dataset.previousKeyType = ''
+            } else {
+                key.textContent = 'AC'
+            }
+
+            display.textContent = '0'
             calculator.dataset.previousKeyType = 'clear'
         }
 
